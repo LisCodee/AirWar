0271f820f45b0a758d07935a24f0a22bf6776866
LisCodee <1376417539@qq.com>
2019-10-15 22:53:40 +0800

    城市模型和小女孩

---
+++ PlaneWar/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 10;
+    public float roundSpeed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, -roundSpeed* Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(0, roundSpeed * Time.deltaTime, 0);
+        }
+    }
+}
